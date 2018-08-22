5e92b74a210f58a3d27ee9aeab25836f93eb693e
Zico Deng <zicodeng@gmail.com>
2018-08-21 22:48:28 -0700

    Correct babel plugin name

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const pkg = require('./package.json');
+const outDir = './dist';
+const context = __dirname;
+
+module.exports = {
+	entry: {
+		[pkg.name]: './src/index.ts',
+	},
+	output: {
+		filename: '[name].min.js',
+		path: path.resolve(__dirname, outDir),
+		library: 'ZtopiaUI',
+		libraryTarget: 'umd',
+		umdNamedDefine: true,
+	},
+	context,
+	resolve: {
+		extensions: [
+			'.js',
+			'.jsx',
+			'.json',
+			'.ts',
+			'.tsx',
+			'.css',
+			'.scss',
+			'.svg',
+			'.jpg',
+		],
+		modules: ['node_modules', 'src'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(t|j)sx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'thread-loader',
+						options: {
+							// There should be 1 cpu for the fork-ts-checker-webpack-plugin.
+							workers: require('os').cpus().length - 1,
+						},
+					},
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [
+								'@babel/preset-env',
+								'@babel/preset-react',
+							],
+							plugins: ['@babel/plugin-transform-runtime'],
+							cacheDirectory: true,
+						},
+					},
+					{
+						loader: 'ts-loader',
+						options: {
+							// IMPORTANT! Use happyPackMode mode to speed-up
+							// compilation and reduce errors reported to webpack.
+							happyPackMode: true,
+						},
+					},
+				],
+			},
+		],
+	},
+};
