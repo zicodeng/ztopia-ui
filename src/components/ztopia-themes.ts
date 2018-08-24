85b62b0fc513fdb4f251c4b4e70a4d8ad48a2591
Zico Deng <zicodeng@gmail.com>
2018-08-24 12:18:03 -0700

    Define Ztopia themes

---
+++ src/components/ztopia-themes.ts
@@ -0,0 +1,62 @@
+/**
+ * Available Ztopia themes.
+ */
+export const ztopiaThemes = ['Basic', 'Space'];
+
+export const isThemeValid = (theme: string): boolean => {
+    if (ztopiaThemes.indexOf(theme) === -1) {
+        throw new Error(
+            `${theme} is an invalid Ztopia theme. Please read Introduction section to view all available Ztopia themes.`,
+        );
+    }
+    return true;
+};
+
+export interface IZtopiaThemes {
+    Basic: any;
+    Space: any;
+    [key: string]: any;
+}
+
+const ZtopiaThemes: IZtopiaThemes = {
+    Basic: {
+        Color: {
+            PRIMARY: '',
+            PRIMARY_LIGHT: '',
+            PRIMARY_DARK: '',
+            ACCENT: '',
+            ACCENT_LIGHT: '',
+            ACCENT_DARK: '',
+            LIGHT: '#FFFFFF',
+            LIGHT_LIGHT: '',
+            LIGHT_DARK: '',
+            DARK: '#000000',
+            DARK_LIGHT: '',
+            DARK_DARK: '',
+        },
+        Animation: {
+            TRANSITION: '',
+        },
+    },
+    Space: {
+        Color: {
+            PRIMARY: '#36E2D2',
+            PRIMARY_LIGHT: '',
+            PRIMARY_DARK: '#2EACCC',
+            ACCENT: '',
+            ACCENT_LIGHT: '',
+            ACCENT_DARK: '',
+            LIGHT: '#FFFFFF',
+            LIGHT_LIGHT: '',
+            LIGHT_DARK: '',
+            DARK: '#1A1A1A',
+            DARK_LIGHT: '#A5A5A5',
+            DARK_DARK: '',
+        },
+        Animation: {
+            TRANSITION: 'all .5s ease',
+        },
+    },
+};
+
+export default ZtopiaThemes;
