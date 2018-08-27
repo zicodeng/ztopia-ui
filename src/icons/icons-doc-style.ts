3a4113931ee1ae3c5938b9460f32eddf39ffb1a9
Zico Deng <zicodeng@gmail.com>
2018-08-27 00:07:22 -0700

    Add icon docs

---
+++ src/icons/icons-doc-style.ts
@@ -0,0 +1,11 @@
+import { css } from 'react-emotion';
+
+export const playgroundStyle = css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    > svg {
+        width: 50px;
+    }
+`;
