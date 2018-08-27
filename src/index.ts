9817c84ac650bfa8ec174e9255228ebba1fce634
Zico Deng <zicodeng@gmail.com>
2018-08-26 23:50:15 -0700

    Export icons

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+// Components
 import Accordion from 'components/accordion/Accordion';
 import Button from 'components/button/Button';
 import Input from 'components/input/Input';
 import Loader from 'components/loader/Loader';
 
 export { Accordion, Button, Input, Loader };
+
+// Icons
+import IconChevronLeft from 'icons/IconChevronLeft';
+import IconUser from 'icons/iconUser';
+
+export { IconChevronLeft, IconUser };
