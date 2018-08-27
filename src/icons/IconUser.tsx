6eb68c79b0a9c4135918339c37c6c7bf8a3f2875
Zico Deng <zicodeng@gmail.com>
2018-08-26 23:47:39 -0700

    Add user icon

--- src/icons/IconUser.tsx
+++ src/icons/IconUser.tsx
@@ -1,26 +1,26 @@
 import * as React from 'react';
 import { cx } from 'react-emotion';
 
-const ChevronLeft: React.SFC<{
+const IconUser: React.SFC<{
     className?: string;
 }> = props => {
     const { className, ...restProps } = props;
-    const classNames = cx('chevron-left', className);
+    const classNames = cx('ztopia-icon-user', className);
     return (
         <svg
             aria-hidden="true"
             className={classNames}
             role="img"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 320 512"
+            viewBox="0 0 448 512"
             {...restProps}
         >
             <path
                 fill="currentColor"
-                d="M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"
+                d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"
             />
         </svg>
     );
 };
 
-export default ChevronLeft;
+export default IconUser;
