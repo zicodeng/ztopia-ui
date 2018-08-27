400cd41b162bc4de6150c62b7556ec1c21eb8b44
Zico Deng <zicodeng@gmail.com>
2018-08-27 00:14:22 -0700

    Export Panel component

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 // Components
 import Accordion from 'components/accordion/Accordion';
+import Panel from 'components/accordion/Panel';
 import Button from 'components/button/Button';
 import Input from 'components/input/Input';
 import Loader from 'components/loader/Loader';
 
-export { Accordion, Button, Input, Loader };
+export { Accordion, Panel, Button, Input, Loader };
 
 // Icons
 import IconChevronLeft from 'icons/IconChevronLeft';
