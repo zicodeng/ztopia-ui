e67e90d2e0c4cb8f205ec8e1a28bc9ca0a1ad73f
Zico Deng <zicodeng@gmail.com>
2019-08-01 15:35:06 -0700

    Add basic button

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,45 @@
-import React, { FC } from 'react';
+import classNames from 'classnames';
+import React, { FC, memo } from 'react';
+
+import './Button.css';
 
 export interface ButtonProps {
-  variant: 'primary' | 'secondary';
+  /**
+   * <@default=`false`>
+   * Ghost buttons are generally bordered by a very thin line,
+   * while the internal section consists of plain text.
+   * They most often appear as Call to Action (CTA) buttons and offer a clean look
+   */
+  ghost?: boolean;
+  /**
+   * <@default=`'rect'`>
+   */
+  variant?: 'rect' | 'pill';
+  /**
+   * <@default=`'medium'`>
+   */
+  size?: 'small' | 'medium' | 'large';
 }
 
-export const Button: FC<ButtonProps> = ({ variant }) => (
-  <button>Hello {variant}</button>
+export const Button: FC<ButtonProps> = memo(
+  ({ ghost, variant, size, children }) => (
+    <button
+      className={classNames(
+        'ztopia-button',
+        `ztopia-button--${variant}`,
+        `ztopia-button--${size}`,
+        {
+          'ztopia-button--ghost': ghost,
+        },
+      )}
+    >
+      {children}
+    </button>
+  ),
 );
 
 Button.defaultProps = {
-  variant: 'primary',
+  ghost: false,
+  variant: 'rect',
+  size: 'medium',
 };
--- components/Card/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC, memo } from 'react';
-
-interface CardProps {
-  title: string;
-}
-
-export const Card: FC<CardProps> = memo(({ title }) => {
-  return <div>{title}</div>;
-});
