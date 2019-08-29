78d5f7680c6fd37eb91ff12c577344b1582d96bf
Zico Deng <zicodeng@gmail.com>
2019-08-29 11:06:09 -0700

    Add Input prefixIcon and suffixIcon props

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -6,6 +6,10 @@ import React, {
   useCallback,
   useEffect,
   useState,
+  ReactNode,
+  useMemo,
+  isValidElement,
+  cloneElement,
 } from 'react';
 
 import './Input.css';
@@ -25,6 +29,8 @@ export interface InputProps {
    * <@default=`'rect'`>
    */
   variant?: 'material' | 'rect' | 'pill';
+  prefixIcon?: ReactNode;
+  suffixIcon?: ReactNode;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
@@ -40,6 +46,8 @@ export const Input = memo(
         error,
         className,
         variant = 'rect',
+        prefixIcon,
+        suffixIcon,
         onChange,
         ...restProps
       },
@@ -60,6 +68,46 @@ export const Input = memo(
       const handleBlurInput = useCallback(() => {
         if (!value && !placeholder) setIsActive(false);
       }, [value]);
+
+      const memoizedPrefixIcon = useMemo(() => {
+        const prefixIconClassName = classNames(
+          'ztopia-input__prefix-icon',
+          `ztopia-input__prefix-icon--${variant}`,
+        );
+        if (typeof prefixIcon === 'function') {
+          const PrefixIcon = prefixIcon;
+          return <PrefixIcon className={prefixIconClassName} />;
+        }
+        if (typeof prefixIcon === 'object' && isValidElement(prefixIcon)) {
+          return cloneElement(prefixIcon, {
+            className: classNames(
+              prefixIcon.props.className,
+              prefixIconClassName,
+            ),
+          });
+        }
+        return null;
+      }, [prefixIcon, variant]);
+
+      const memoizedSuffixIcon = useMemo(() => {
+        const suffixIconClassName = classNames(
+          'ztopia-input__suffix-icon',
+          `ztopia-input__suffix-icon--${variant}`,
+        );
+        if (typeof suffixIcon === 'function') {
+          const SuffixIcon = suffixIcon;
+          return <SuffixIcon className={suffixIconClassName} />;
+        }
+        if (typeof suffixIcon === 'object' && isValidElement(suffixIcon)) {
+          return cloneElement(suffixIcon, {
+            className: classNames(
+              suffixIcon.props.className,
+              suffixIconClassName,
+            ),
+          });
+        }
+        return null;
+      }, [suffixIcon, variant]);
 
       return (
         <div
@@ -82,32 +130,38 @@ export const Input = memo(
               {label}
             </label>
           )}
-          <input
-            {...restProps}
-            autoComplete="on"
-            ref={ref}
-            disabled={isDisabled}
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            className={classNames(
-              'ztopia-input__input',
-              `ztopia-input__input--${variant}`,
-              {
-                'has-error': Boolean(error),
-              },
-            )}
-            onChange={onChange}
-            onFocus={handleFocusInput}
-            onBlur={handleBlurInput}
-          />
-          {variant === 'material' && (
-            <div
-              className={classNames('ztopia-input__bar', {
-                'has-error': Boolean(error),
-              })}
+          <div className="ztopia-input__input-container">
+            {memoizedPrefixIcon && memoizedPrefixIcon}
+            <input
+              {...restProps}
+              autoComplete="on"
+              ref={ref}
+              disabled={isDisabled}
+              type={type}
+              value={value}
+              placeholder={placeholder}
+              className={classNames(
+                'ztopia-input__input',
+                `ztopia-input__input--${variant}`,
+                {
+                  'has-error': Boolean(error),
+                  'has-prefix-icon': Boolean(prefixIcon),
+                  'has-suffix-icon': Boolean(suffixIcon),
+                },
+              )}
+              onChange={onChange}
+              onFocus={handleFocusInput}
+              onBlur={handleBlurInput}
             />
-          )}
+            {variant === 'material' && (
+              <div
+                className={classNames('ztopia-input__bar', {
+                  'has-error': Boolean(error),
+                })}
+              />
+            )}
+            {memoizedSuffixIcon && memoizedSuffixIcon}
+          </div>
           {error && <span className="ztopia-input__error">{error}</span>}
         </div>
       );
