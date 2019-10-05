5bf3450f415306f34eb5e1c61ff791a78a87ac76
Zico Deng <zicodeng@gmail.com>
2019-10-05 15:48:23 -0700

    Add Typewriter

---
+++ components/Typewriter/Typewriter.tsx
@@ -0,0 +1,41 @@
+import React, { FC, memo } from 'react';
+import classNames from 'classnames';
+import Typed from 'react-typed';
+
+export interface TypewriterProps {
+  /**
+   * <@default=`false`>
+   */
+  isLooped?: boolean;
+  /**
+   * <@default=`60`>
+   */
+  typeSpeed?: number;
+  /**
+   * <@default=`80`>
+   */
+  backSpeed?: number;
+  backDelay?: number;
+  className?: string;
+  text?: string[];
+}
+
+export const Typewriter: FC<TypewriterProps> = memo(
+  ({
+    isLooped = false,
+    typeSpeed = 60,
+    backSpeed = 80,
+    className,
+    text,
+    ...restProps
+  }) => (
+    <Typed
+      loop={isLooped}
+      typeSpeed={typeSpeed}
+      backSpeed={backSpeed}
+      className={classNames('ztopia-typewriter', className)}
+      strings={text}
+      {...restProps}
+    />
+  ),
+);
