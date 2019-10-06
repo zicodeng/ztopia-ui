6622b9af19d35199582d875af997b97288fa8fd0
Zico Deng <zicodeng@gmail.com>
2019-10-06 13:51:52 -0700

    Change SkewedEdge default direction

---
+++ components/SkewedEdge/SkewedEdge.tsx
@@ -0,0 +1,30 @@
+import React, { FC, CSSProperties, memo } from 'react';
+import classNames from 'classnames';
+
+import './SkewedEdge.css';
+
+export interface SkewedEdgeProps {
+  className?: string;
+  style?: CSSProperties;
+  /**
+   * <@default=`'left-to-right'`>
+   */
+  direction?:
+    | 'left-to-right'
+    | 'right-to-left'
+    | 'top-to-bottom'
+    | 'bottom-to-top';
+}
+
+export const SkewedEdge: FC<SkewedEdgeProps> = memo(
+  ({ className, direction = 'left-top-right', ...restProps }) => (
+    <div
+      className={classNames(
+        className,
+        'ztopia-skewed-edge',
+        `ztopia-skewed-edge--${direction}`,
+      )}
+      {...restProps}
+    />
+  ),
+);
