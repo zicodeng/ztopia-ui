ab324981a040aa22d82fc701f34d819ec95968c9
Zico Deng <zicodeng@gmail.com>
2019-10-11 15:35:26 -0700

    Add waiting chat bubble

--- components/WaitingChatBubble/WaitingChatBubble.tsx
+++ components/WaitingChatBubble/WaitingChatBubble.tsx
@@ -1,18 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, memo } from 'react';
 import classNames from 'classnames';
+
+import { FadingLoader } from '../Loaders';
 
 import './WaitingChatBubble.css';
 
 export interface WaitingChatBubbleProps {
   className?: string;
+  size?: 'small' | 'medium' | 'large';
 }
 
-export const WaitingChatBubble: FC<WaitingChatBubbleProps> = ({
-  className,
-}) => (
-  <div className={classNames(className, 'ztopia-waiting-chat-bubble')}>
-    <div className="ztopia-waiting-chat-bubble__circle"></div>
-    <div className="ztopia-waiting-chat-bubble__circle"></div>
-    <div className="ztopia-waiting-chat-bubble__circle"></div>
-  </div>
+export const WaitingChatBubble: FC<WaitingChatBubbleProps> = memo(
+  ({ className, size = 'medium' }) => (
+    <div
+      className={classNames(
+        className,
+        'ztopia-waiting-chat-bubble',
+        `ztopia-waiting-chat-bubble--${size}`,
+      )}
+    >
+      <FadingLoader color="#bbb" size="large" />
+    </div>
+  ),
 );
