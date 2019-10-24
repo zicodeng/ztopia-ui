e2fbf994494acf3637477f81c4ca67623f0ef278
Zico Deng <zicodeng@gmail.com>
2019-10-24 13:31:40 -0700

    chore: customize commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'revert',
+        'improvement',
+        'perf',
+        'build',
+        'ci',
+        'chore',
+      ],
+    ],
+  },
 };
