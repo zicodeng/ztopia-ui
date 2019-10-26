7a2fce4f45f3df9feff424c302ea5f2aed35331e
Zico Deng <zicodeng@gmail.com>
2019-10-25 21:21:19 -0700

    improvement: add color prop to FloatingBoxes

--- components/FloatingBoxes/FloatingBoxes.tsx
+++ components/FloatingBoxes/FloatingBoxes.tsx
@@ -5,15 +5,25 @@ import './FloatingBoxes.css';
 
 export interface FloatingBoxesProps {
   className?: string;
+  /**
+   * <@default=`#46cead`>
+   */
+  color?: string;
 }
 
-export const FloatingBoxes: FC<FloatingBoxesProps> = ({ className }) => (
+export const FloatingBoxes: FC<FloatingBoxesProps> = ({
+  className,
+  color = '#46cead',
+}) => (
   <section className={classNames(className, 'ztopia-floating-boxes')}>
-    <div className="ztopia-floating-boxes__box" />
-    <div className="ztopia-floating-boxes__box" />
-    <div className="ztopia-floating-boxes__box" />
-    <div className="ztopia-floating-boxes__box" />
-    <div className="ztopia-floating-boxes__box" />
-    <div className="ztopia-floating-boxes__box" />
+    {Array.from({ length: 6 }).map((_, i) => (
+      <div
+        key={i}
+        className="ztopia-floating-boxes__box"
+        style={{
+          backgroundColor: color,
+        }}
+      />
+    ))}
   </section>
 );
