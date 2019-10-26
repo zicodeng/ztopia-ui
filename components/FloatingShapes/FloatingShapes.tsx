5e69ef7847de664acbccfb50353cc0e3aaef791a
Zico Deng <zicodeng@gmail.com>
2019-10-25 21:41:00 -0700

    fix: rename FloatingBoxes to FloatingShapes

---
+++ components/FloatingShapes/FloatingShapes.tsx
@@ -0,0 +1,30 @@
+import React, { FC, memo } from 'react';
+import classNames from 'classnames';
+
+import './FloatingShapes.css';
+
+export interface FloatingShapesProps {
+  className?: string;
+  /**
+   * <@default=`#46cead`>
+   */
+  color?: string;
+  variant?: 'rect' | 'circle';
+}
+
+export const FloatingShapes: FC<FloatingShapesProps> = memo(
+  ({ className, color = '#46cead', variant = 'rect' }) => (
+    <section className={classNames(className, 'ztopia-floating-boxes')}>
+      {Array.from({ length: 6 }).map((_, i) => (
+        <div
+          key={i}
+          className="ztopia-floating-boxes__box"
+          style={{
+            backgroundColor: color,
+            borderRadius: variant === 'rect' ? 0 : '50%',
+          }}
+        />
+      ))}
+    </section>
+  ),
+);
