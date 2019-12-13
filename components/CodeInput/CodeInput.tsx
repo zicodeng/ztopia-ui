d515d8fb616926a0c0aa2cdfe4fcad1848700ed0
Zico Deng <zicodeng@gmail.com>
2019-12-13 11:06:08 -0800

    feat: implement CodeInput component

---
+++ components/CodeInput/CodeInput.tsx
@@ -0,0 +1,33 @@
+import React, { memo, FC } from 'react';
+import ReactCodeInput from 'react-code-input';
+import classNames from 'classnames';
+
+import './CodeInput.css';
+
+export interface CodeInputProps {
+  /**
+   * <@default=`6`>
+   */
+  totalFields?: number;
+  value?: string;
+  name?: string;
+  className?: string;
+  /**
+   * <@default=`text`>
+   */
+  type?: 'text' | 'number' | 'password';
+  onChange?: (newValue: string) => void;
+}
+
+export const CodeInput: FC<CodeInputProps> = memo(
+  ({ totalFields = '6', value, name, className, type = 'text', onChange }) => (
+    <ReactCodeInput
+      fields={totalFields}
+      value={value}
+      name={name}
+      type={type}
+      className={classNames(className, 'ztopia-code-input')}
+      onChange={onChange}
+    />
+  ),
+);
---
+++ components/CodeInput/CodeInputDemo.jsx
@@ -0,0 +1,10 @@
+import { memo, useState } from 'react';
+
+export const CodeInputDemo = memo(({ children }) => {
+  const [value, setValue] = useState('');
+  const onChange = newValue => {
+    console.log('CodeInput', 'onChange', { newValue });
+    setValue(newValue);
+  };
+  return children({ value, onChange });
+});
