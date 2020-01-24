f8ee175a9434c9646f07497a989ee7765b9fdf57
Zico Deng <zicodeng@gmail.com>
2020-01-24 11:59:12 -0800

    fix: use hex-rgb library to do color conversion

--- components/Loaders/BasicLoader.tsx
+++ components/Loaders/BasicLoader.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames';
 import React, { FC, useMemo } from 'react';
+import hexRgb from 'hex-rgb';
 
 import { Loader, LoaderProps } from '.';
 
@@ -8,21 +9,12 @@ import './BasicLoader.css';
 export const BasicLoader: FC<LoaderProps> = props => (
   <Loader {...props}>
     {({ color, className }) => {
-      const { r, g, b } = useMemo(() => {
-        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
-        return result
-          ? {
-              r: parseInt(result[1], 16),
-              g: parseInt(result[2], 16),
-              b: parseInt(result[3], 16),
-            }
-          : { r: 0, g: 0, b: 0 };
-      }, [color]);
+      const { red, green, blue } = useMemo(() => hexRgb(color), [color]);
       return (
         <div
           className={classNames(className, 'ztopia-loader--basic')}
           style={{
-            borderColor: `rgba(${r}, ${g}, ${b}, 0.2)`,
+            borderColor: `rgba(${red}, ${green}, ${blue}, 0.2)`,
             borderTopColor: color,
           }}
         />
