83a7eb6e9bf7ba55eacc9bf151e064583c037eaa
Zico Deng <zicodeng@gmail.com>
2020-01-26 10:10:16 -0800

    fix: add gradient rendering support to Progress

--- components/Progress/Progress.tsx
+++ components/Progress/Progress.tsx
@@ -1,11 +1,17 @@
 import classNames from 'classnames';
 import { Circle, Line } from 'rc-progress';
-import React, { CSSProperties, FC } from 'react';
+import React, { CSSProperties, FC, memo } from 'react';
 
 import 'rc-progress/assets/index.css';
 import './Progress.css';
 
 export interface ProgressProps {
+  /**
+   * Render color from red to green based on percent value
+   *
+   * <@default=`false`>
+   */
+  isGradientEnabled?: boolean;
   /**
    * Value must be in range of 0 to 100
    */
@@ -35,32 +41,42 @@ export interface ProgressProps {
   style?: CSSProperties;
 }
 
-export const Progress: FC<ProgressProps> = ({
-  percent,
-  strokeWidth,
-  trailWidth,
-  strokeColor,
-  trailColor,
-  className,
-  variant,
-  style,
-}) => {
-  const props = {
-    percent,
-    strokeWidth,
-    trailWidth,
-    strokeColor,
-    trailColor,
-    style,
-    className: classNames(className, `ztopia-progress--${variant}`),
-  };
-  return variant === 'line' ? <Line {...props} /> : <Circle {...props} />;
+const calcGradientColor = percent => {
+  let r = 0;
+  let g = 0;
+  let b = 0;
+  if (percent < 50) {
+    r = 255;
+    g = Math.round(5.1 * percent);
+  } else {
+    g = 255;
+    r = Math.round(510 - 5.1 * percent);
+  }
+  const h = r * 0x10000 + g * 0x100 + b * 0x1;
+  return '#' + ('000000' + h.toString(16)).slice(-6);
 };
 
-Progress.defaultProps = {
-  strokeWidth: 1,
-  trailWidth: 1,
-  strokeColor: '#131518',
-  trailColor: '#f2f3f5',
-  variant: 'line',
-};
+export const Progress: FC<ProgressProps> = memo(
+  ({
+    isGradientEnabled = false,
+    percent,
+    strokeWidth = 1,
+    trailWidth = 1,
+    strokeColor = '#131518',
+    trailColor = '#f2f3f5',
+    className,
+    variant = 'line',
+    style,
+  }) => {
+    const props = {
+      percent,
+      strokeWidth,
+      trailWidth,
+      strokeColor: isGradientEnabled ? calcGradientColor(percent) : strokeColor,
+      trailColor,
+      style,
+      className: classNames(className, `ztopia-progress--${variant}`),
+    };
+    return variant === 'line' ? <Line {...props} /> : <Circle {...props} />;
+  },
+);
