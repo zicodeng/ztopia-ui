3b9c34202cfc606d6e1134cbaa2d5da9d738e0c9
Zico Deng <zicodeng@gmail.com>
2020-03-04 21:15:25 -0800

    fix: remove redundant isFocused state in Input

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -62,17 +62,15 @@ export const Input = memo(
       },
       ref,
     ) => {
-      const [isFocused, setIsFocused] = useState(false);
       const [isActive, setIsActive] = useState(
         Boolean(placeholder || value || defaultValue),
       );
 
       useEffect(() => {
-        setIsActive(Boolean(isFocused || placeholder || value || defaultValue));
-      }, [isFocused, placeholder, value, defaultValue]);
+        setIsActive(Boolean(placeholder || value || defaultValue));
+      }, [placeholder, value, defaultValue]);
 
       const handleFocusInput = useCallback(() => {
-        setIsFocused(true);
         setIsActive(true);
       }, []);
 
