935adb7e59880ef02cd55ca0b4002e9e6a69c5dc
Zico Deng <zicodeng@gmail.com>
2020-04-05 14:31:29 -0700

    fix: add error prop to Textarea

--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -22,18 +22,23 @@ export const Textarea: FC<TextareaProps> = memo(props => (
 export interface InnerTextareaProps {
   minRows?: number;
   maxRows?: number;
+  error?: string;
   /**
    * <@internal>
    */
   className?: Pick<TextareaProps, 'className'>;
 }
 
-export const InnerTextarea: FC<InnerTextareaProps> = ({
-  className,
-  ...restProps
-}) => (
-  <TextareaAutosize
-    className={classNames(className, 'ztopia-textarea')}
-    {...restProps}
-  />
+export const InnerTextarea: FC<InnerTextareaProps> = memo(
+  ({ error, className, ...restProps }) => (
+    <div className={classNames(className, 'ztopia-textarea')}>
+      <TextareaAutosize
+        className={classNames(className, 'ztopia-textarea__input', {
+          'has-error': Boolean(error),
+        })}
+        {...restProps}
+      />
+      {error && <span className="ztopia-textarea__error">{error}</span>}
+    </div>
+  ),
 );
