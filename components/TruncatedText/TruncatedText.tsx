f5eac5318e4f8de1c731173871cce18e5bb14d38
Zico Deng <zicodeng@gmail.com>
2020-06-02 00:00:18 -0700

    fix: add readMoreIndicator prop

--- components/TruncatedText/TruncatedText.tsx
+++ components/TruncatedText/TruncatedText.tsx
@@ -1,6 +1,16 @@
-import React, { FC } from 'react';
+import React, {
+  cloneElement,
+  FC,
+  isValidElement,
+  ReactNode,
+  useCallback,
+  useState,
+} from 'react';
 import LinesEllipsis from 'react-lines-ellipsis';
 import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC';
+import classNames from 'classnames';
+
+import './TruncatedText.css';
 
 const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
@@ -9,6 +19,10 @@ export interface TruncatedText {
    * <@default=`2`>
    */
   maxLine: number;
+  /**
+   * <@default=`... read more`>
+   */
+  readMoreIndicator?: ReactNode;
   /**
    * HTML element the truncated text should be rendered as
    *
@@ -21,13 +35,48 @@ export interface TruncatedText {
 export const TruncatedText: FC<TruncatedText> = ({
   className,
   maxLine,
+  readMoreIndicator = null,
   element = 'p',
   children,
-}) => (
-  <ResponsiveEllipsis
-    className={className}
-    text={children}
-    maxLine={maxLine}
-    component={element}
-  />
-);
+}) => {
+  const [isTruncated, setIsTruncated] = useState<boolean>(true);
+
+  const handleReadMoreIndicatorClick = useCallback(() => {
+    setIsTruncated(false);
+  }, []);
+
+  return isTruncated ? (
+    <ResponsiveEllipsis
+      className={classNames(className, 'ztopia-truncated-text')}
+      text={children}
+      maxLine={maxLine}
+      ellipsis={
+        readMoreIndicator ? (
+          isValidElement(readMoreIndicator) ? (
+            cloneElement(readMoreIndicator, {
+              // @ts-ignore
+              onClick: handleReadMoreIndicatorClick,
+              className: classNames(
+                // @ts-ignore
+                readMoreIndicator.props.className,
+                'ztopia-truncated-text__read-more-indicator',
+              ),
+            })
+          ) : (
+            <span
+              className="ztopia-truncated-text__read-more-indicator"
+              onClick={handleReadMoreIndicatorClick}
+            >
+              {readMoreIndicator}
+            </span>
+          )
+        ) : (
+          undefined
+        )
+      }
+      component={element}
+    />
+  ) : (
+    <p className={className}>{children}</p>
+  );
+};
