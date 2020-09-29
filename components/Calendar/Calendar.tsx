75e4d72993ebf80f05727b28aa6bb0dff914aedf
Zico Deng <zicodeng@gmail.com>
2020-09-28 21:48:35 -0700

    fix: add calendar locale support

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,12 +1,25 @@
 import React, { FC } from 'react';
-import { Calendar as BaseCalendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar as BaseCalendar, dateFnsLocalizer } from 'react-big-calendar';
 import classNames from 'classnames';
-import moment from 'moment';
+import { format, getDay,parse, startOfWeek } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+import zhCN from 'date-fns/locale/zh-CN';
 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './Calendar.css';
 
-const localizer = momentLocalizer(moment);
+const locales = {
+  'en-US': enUS,
+  'zh-CN': zhCN,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 export interface Event {
   allDay?: boolean;
@@ -30,6 +43,10 @@ export interface CalendarProps {
   isSelectable?: boolean;
   className?: string;
   events?: Event[];
+  /**
+   * <@default=`en-US`>
+   */
+  locale?: 'en-US' | 'zh-CN';
   onSelectSlot?: (slot: Slot) => void;
   onSelectEvent?: (event: Event, e: React.SyntheticEvent<HTMLElement>) => void;
 }
@@ -37,6 +54,7 @@ export interface CalendarProps {
 export const Calendar: FC<CalendarProps> = ({
   isSelectable,
   className,
+  locale = 'en-US',
   ...restProps
 }) => (
   <BaseCalendar
@@ -44,6 +62,22 @@ export const Calendar: FC<CalendarProps> = ({
     selectable={isSelectable}
     className={classNames(className, 'ztopia-calendar')}
     localizer={localizer}
+    culture={locale}
+    messages={
+      locale === 'zh-CN'
+        ? {
+            today: '今天',
+            next: '>',
+            previous: '<',
+            month: '月',
+            week: '周',
+            day: '日',
+            agenda: '议程',
+            showMore: count => `查看更多(${count}+)`,
+            allDay: '全天',
+          }
+        : undefined
+    }
   />
 );
 
--- components/Calendar/CalendarDemo.jsx
+++ components/Calendar/CalendarDemo.jsx
@@ -2,14 +2,17 @@ import { memo, useState } from 'react';
 
 export const CalendarDemo = memo(({ children }) => {
   const [events, setEvents] = useState([]);
+
   const onSelectEvent = event => {
     console.log('Calender', 'onSelectEvent', { event });
     window.alert(event.title);
   };
+
   const onSelectSlot = slot => {
     console.log('Calendar', 'onSelectSlot', { slot });
     const title = window.prompt('Event Title');
     if (!title) return;
+
     const newEvent = {
       title,
       start: slot.start,
@@ -17,5 +20,6 @@ export const CalendarDemo = memo(({ children }) => {
     };
     setEvents([...events, newEvent]);
   };
+
   return children({ events, onSelectEvent, onSelectSlot });
 });
