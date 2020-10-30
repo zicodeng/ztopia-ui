3fc426dc2347b3d7c01549711a6662d5530eef52
Zico Deng <zicodeng@gmail.com>
2020-10-29 22:52:01 -0700

    fix: add element and itemElement props to GridList

--- components/GridList/GridList.tsx
+++ components/GridList/GridList.tsx
@@ -2,21 +2,43 @@ import React, { memo, ReactNode } from 'react';
 import StackGrid from 'react-stack-grid';
 import classNames from 'classnames';
 
+import './GridList.css';
+
 export interface GridListProps {
   /**
    * <@default=`5`>
    */
   gap?: number;
+  /**
+   * <@default=`'ul'`>
+   */
+  element?: string;
+  /**
+   * <@default=`'li'`>
+   */
+  itemElement?: string;
   className?: string;
   children?: ReactNode;
-  columnWidth: string | number;
+  /**
+   * <@default=`200`>
+   */
+  columnWidth?: string | number;
 }
 
 export const GridList = memo<GridListProps>(
-  ({ gap = 5, className, children, columnWidth }) => (
+  ({
+    gap = 5,
+    element = 'ul',
+    itemElement = 'li',
+    className,
+    children,
+    columnWidth = 200,
+  }) => (
     <StackGrid
       gutterWidth={gap}
       gutterHeight={gap}
+      component={element}
+      itemComponent={itemElement}
       columnWidth={columnWidth}
       className={classNames('ztopia-grid-list', className)}
     >
