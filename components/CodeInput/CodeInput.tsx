e5feae4dfc623cbc380ffda186677f9d9056acd9
Zico Deng <zicodeng@gmail.com>
2021-01-31 13:29:47 -0800

    fix: add inputMode prop to CodeInput

--- components/CodeInput/CodeInput.tsx
+++ components/CodeInput/CodeInput.tsx
@@ -10,22 +10,47 @@ export interface CodeInputProps {
    */
   totalFields?: number;
   value?: string;
-  name?: string;
+  name: string;
   className?: string;
   /**
    * <@default=`text`>
    */
   type?: 'text' | 'number' | 'password';
+  /**
+   * <@default=`numeric`>
+   */
+  inputMode?:
+    | 'verbatim'
+    | 'latin'
+    | 'latin-name'
+    | 'latin-prose'
+    | 'full-width-latin'
+    | 'kana'
+    | 'kana-name'
+    | 'katakana'
+    | 'numeric'
+    | 'tel'
+    | 'email'
+    | 'url';
   onChange?: (newValue: string) => void;
 }
 
 export const CodeInput = memo<CodeInputProps>(
-  ({ totalFields = '6', value, name, className, type = 'text', onChange }) => (
+  ({
+    totalFields = 6,
+    value,
+    name,
+    className,
+    type = 'text',
+    inputMode = 'numeric',
+    onChange,
+  }) => (
     <ReactCodeInput
       fields={totalFields}
       value={value}
       name={name}
       type={type}
+      inputMode={inputMode}
       className={classNames(className, 'ztopia-code-input')}
       onChange={onChange}
     />
