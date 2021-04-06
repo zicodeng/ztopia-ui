a25e12852d82966d549e51e98e01e32fb5d531ca
Zico Deng <zicodeng@gmail.com>
2021-04-05 21:07:38 -0700

    fix: use static id

--- components/FloatingActionButton/FloatingActionButton.tsx
+++ components/FloatingActionButton/FloatingActionButton.tsx
@@ -1,6 +1,5 @@
 import React, { memo, ReactNode } from 'react';
 import classNames from 'classnames';
-import uuidv4 from 'uuid/v4';
 
 import { Popper } from '../Popper';
 
@@ -19,45 +18,44 @@ export interface FloatingActionButtonProps {
   actions: Action[];
 }
 
+const ID = 'ztopia-fab';
+
 export const FloatingActionButton = memo<FloatingActionButtonProps>(
-  ({ offset = 50, className, size = 'medium', actions }) => {
-    const id = uuidv4();
-    return (
-      <div
-        id={id}
-        className={classNames(className, 'ztopia-fab', `ztopia-fab--${size}`)}
-        style={{
-          right: offset,
-          bottom: offset,
-        }}
+  ({ offset = 50, className, size = 'medium', actions }) => (
+    <div
+      id={ID}
+      className={classNames(className, 'ztopia-fab', `ztopia-fab--${size}`)}
+      style={{
+        right: offset,
+        bottom: offset,
+      }}
+    >
+      <ul
+        className={classNames(
+          'ztopia-fab__actions',
+          `ztopia-fab__actions--${size}`,
+        )}
       >
-        <ul
-          className={classNames(
-            'ztopia-fab__actions',
-            `ztopia-fab__actions--${size}`,
-          )}
-        >
-          {actions.map(({ icon, desc, onClick }, i) => (
-            <li key={i} className="ztopia-fab__action" onClick={onClick}>
-              <Popper
-                containerId={id}
-                offsetX={-5}
-                placement="left"
-                overlay={<span>{desc}</span>}
+        {actions.map(({ icon, desc, onClick }, i) => (
+          <li key={i} className="ztopia-fab__action" onClick={onClick}>
+            <Popper
+              containerId={ID}
+              offsetX={-5}
+              placement="left"
+              overlay={<span>{desc}</span>}
+            >
+              <div
+                className={classNames(
+                  'ztopia-fab__action-content',
+                  `ztopia-fab__action-content--${size}`,
+                )}
               >
-                <div
-                  className={classNames(
-                    'ztopia-fab__action-content',
-                    `ztopia-fab__action-content--${size}`,
-                  )}
-                >
-                  {icon}
-                </div>
-              </Popper>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  },
+                {icon}
+              </div>
+            </Popper>
+          </li>
+        ))}
+      </ul>
+    </div>
+  ),
 );
